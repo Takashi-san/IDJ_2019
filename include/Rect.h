6e98280f8e75bbaa3de814acf13aad7899a8042c
Unknown <bt.tengan@gmail.com>
2019-04-04 16:58:44 -0300

    Implementação básica classes Vec2 e Rect
    Implementação só com suas variáveis, ainda não possuem nenhum método.

---
+++ include/Rect.h
@@ -0,0 +1,8 @@
+#ifndef RECT
+	class Rect {
+	public:
+		float x, y, w, h;
+
+	};
+	#define RECT
+#endif
---
+++ include/Vec2.h
@@ -0,0 +1,8 @@
+#ifndef VEC2
+	class Vec2 {
+	public:
+		float x, y;
+
+	};
+	#define VEC2
+#endif
